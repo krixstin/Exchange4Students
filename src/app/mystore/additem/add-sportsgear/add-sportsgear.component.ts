74d1b3c7e636926b99b312346e7201474efca1a0
Kristin Kim <kristinhajinkim@gmail.com>
2021-04-13 04:06:58 -0400

    reactive forms completed

--- src/app/mystore/additem/add-sportsgear/add-sportsgear.component.ts
+++ src/app/mystore/additem/add-sportsgear/add-sportsgear.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { ngModuleJitUrl } from '@angular/compiler';
+import { ViewEncapsulation } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+// import {AngularFireDatabase, FirebaseObjectObservable} from 'angularfire2/database';
 
 @Component({
   selector: 'app-add-sportsgear',
@@ -7,9 +14,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddSportsgearComponent implements OnInit {
 
-  constructor() { }
+  constructor(public router: Router,
+    private fb: FormBuilder, 
+    private firestore: AngularFirestore){}
+    private submissionForm!: AngularFirestoreCollection<any[]>
 
-  ngOnInit(): void {
+  ourForm = new FormGroup({
+    type: new FormControl('clothingtype'),
+    color: new FormControl('color'),
+    size: new FormControl('size'),
+
+    description :new FormControl('description'),
+    price: new FormControl('price'),
+    sellerid: new FormControl('sellerid'),
+    picture:new FormControl('picture'),
+    shipping: new FormControl('shipping'),
+
+  });  
+
+  ngOnInit(): void{
+    // this.initializeForm();
+    this.submissionForm=this.firestore.collection('items');
+    this.ourForm = this.fb.group({
+
+    type: [''],
+    weight: [''],
+
+    itemID:[''],
+    description : ['' ],
+    price: [''],
+    sellerid: [''],
+    category: ['sportsgear'],
+    picture:[''],
+    shipping: ['']
+    });
   }
 
+  submitForm(value: any){
+    console.log(this.fb.control)
+    this.submissionForm.add(value).then(res=>{
+      console.log('item added!');
+      }).catch(err=> console.log(err)
+      );
+  }
 }
