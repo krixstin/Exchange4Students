13a171df78d35b6c993a31bb16bd06a787d37976
RedRoach51 <RedRoach51@gmail.com>
2021-04-13 18:12:42 -0400

    Category pages now only stores items of corresponding category. (Only viewable in console)

--- src/app/browse/br-furniture/br-furniture.component.ts
+++ src/app/browse/br-furniture/br-furniture.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { item } from '../../Item';
+import { ItemService } from '../../item.service';
+import { Mock } from '../../MockBook';
 
 @Component({
   selector: 'app-br-furniture',
@@ -7,10 +10,40 @@ import { Router } from '@angular/router';
   styleUrls: ['./br-furniture.component.css']
 })
 export class BrFurnitureComponent implements OnInit {
+  retrievedItems: item[];
+  items: item[];
 
-  constructor(public router: Router) { }
 
+  constructor(public router: Router, 
+             private ItemService: ItemService) { }
+
+
+  getItems():void{
+  	this.ItemService.getItems()
+  		.subscribe(items => this.retrievedItems = items)
+
+    var removedItems: number[] = [];
+
+    console.log(this.retrievedItems)
+    for (const index in this.retrievedItems){
+      var item:item = this.retrievedItems[index]
+        if (item.category !== "furniture"){
+          removedItems.push(parseInt(index))
+        }
+    }
+
+    this.items = [];
+    for (const index in this.retrievedItems){
+      if (!removedItems.includes(parseInt(index))){
+        this.items.push(this.retrievedItems[index])
+      }
+    }
+
+    console.log(this.items)
+  }
+ 
   ngOnInit(): void {
+  	this.getItems();
   }
 
 }
