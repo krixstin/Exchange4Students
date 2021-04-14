4a42f9089698c13856cd7710006adde7702eacba
RedRoach51 <RedRoach51@gmail.com>
2021-04-13 23:42:45 -0400

    Working on 'Read from Database' functionality [Incomplete]

--- src/app/item.service.ts
+++ src/app/item.service.ts
@@ -1,7 +1,15 @@
 import { Injectable } from '@angular/core';
 import { Observable, of} from 'rxjs';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 
-import { item } from './Item';
+import {item} from './Item';
+import {itemSport} from './ItemSport';
+import {itemBook} from './ItemBook';
+import {itemClothing} from './ItemClothing';
+import {itemElectronic} from './ItemElectronic';
+import {itemFurniture} from './ItemFurniture';
+
 import { Mock } from './MockBook';
 
 @Injectable({
@@ -9,12 +17,27 @@ import { Mock } from './MockBook';
 })
 export class ItemService {
 
-  constructor() { }
+  private storedDatabase: AngularFirestoreCollection<any[]>
+  items$: Observable<item[]>
+  constructor(private firestore: AngularFirestore){}
 
   getItems(): Observable<item[]>{
   	// Currently returns Mock Item list 
   	// TODO: Implement get from database
-  	const items = of(Mock)
+    this.storedDatabase = this.firestore.collection('items');
+    this.items$ = this.firestore.collection<item>('items', (ref) => 
+        ref
+      )
+      .valueChanges({idField: 'id'});
+
+    var tempo:item[] = [];
+    const snapshot = this.items$.subscribe(temp => tempo = temp);
+    console.log(snapshot)
+    var retrievedItems;
+    this.storedDatabase.get().subscribe(items => console.log(items));
+  	console.log("Firestore Database")
+    console.log(tempo)
+    const items = of(Mock)
   	return items;
   }
 
