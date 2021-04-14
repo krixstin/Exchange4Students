7c395287172241cc99542bc825d5e6dd30f83e60
Kristin Kim <kristinhajinkim@gmail.com>
2021-04-14 02:31:07 -0400

    can uploade picture to Firebase Storage now, but need to enable texts& pic submission

--- src/app/mystore/additem/add-sportsgear/add-sportsgear.component.ts
+++ src/app/mystore/additem/add-sportsgear/add-sportsgear.component.ts
@@ -2,9 +2,11 @@ import { ngModuleJitUrl } from '@angular/compiler';
 import { ViewEncapsulation } from '@angular/core';
 import { Component, Injectable, OnInit } from '@angular/core';
 import { AngularFireModule } from '@angular/fire';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection, validateEventsArray } from '@angular/fire/firestore';
+import { AngularFireStorage } from '@angular/fire/storage';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import {finalize}from "rxjs/operators"
 // import {AngularFireDatabase, FirebaseObjectObservable} from 'angularfire2/database';
 
 @Component({
@@ -14,43 +16,49 @@ import { Router } from '@angular/router';
 })
 export class AddSportsgearComponent implements OnInit {
 
-  imgSrc: string ="../../../../assets/images/CategorySportGear.jpg";
-  selectedImage: any = null;
+  imgSrc!: string;
+  selectedImage: any ;
+  isSubmitted: boolean = false;
 
   constructor(public router: Router,
     private fb: FormBuilder, 
-    private firestore: AngularFirestore){}
-    private submissionForm!: AngularFirestoreCollection<any[]>
+    private firestore: AngularFirestore,
+    private storage: AngularFireStorage,
+    ){}
 
+    private submissionForm!: AngularFirestoreCollection<any[]>
+    
   ourForm = new FormGroup({
-    type: new FormControl('type'),
-    color: new FormControl('color'),
-    size: new FormControl('size'),
+    type: new FormControl('type', Validators.required),
+    weight: new FormControl('weight'),
+    category: new FormControl('category'),
 
     description :new FormControl('description'),
     price: new FormControl('price'),
     sellerid: new FormControl('sellerid'),
-    picture:new FormControl('picture'),
+    picture:new FormControl('picture', Validators.required),
     shipping: new FormControl('shipping'),
 
   });  
 
   ngOnInit(): void{
+    this.resetForm();
+    
     // this.initializeForm();
-    this.submissionForm=this.firestore.collection('items');
-    this.ourForm = this.fb.group({
+    // this.submissionForm=this.firestore.collection('items');
+    // this.ourForm = this.fb.group({
 
-    type: [''],
-    weight: [''],
+    // type: [''],
+    // weight: [''],
 
-    itemID:[''],
-    description : ['' ],
-    price: [''],
-    sellerid: [''],
-    category: ['sportsgear'],
-    picture:[''],
-    shipping: ['']
-    });
+    // itemID:[''],
+    // description : ['' ],
+    // price: [''],
+    // sellerid: [''],
+    // category: ['sportsgear'],
+    // picture:[''],
+    // shipping: ['']
+    // });
   }
 
   showPreview(event:any){
@@ -70,11 +78,51 @@ export class AddSportsgearComponent implements OnInit {
 
   submitForm(value: any){
     console.log(this.fb.control)
+    this.isSubmitted=true;
+   if(this.ourForm.valid){
+     
+     var filePath = `${this.selectedImage.name}_${new Date().getTime()})`
+     const fileRef = this.storage.ref(filePath);
+     console.log("this is file path",filePath);
+     this.storage.upload(filePath, this.selectedImage).snapshotChanges().pipe(
+      finalize(()=>{
+        fileRef.getDownloadURL().subscribe((url)=>{
+          value['picture']=url;
+          this.resetForm;
+        })
+      })
+     ).subscribe();
+   }
+    // this.submissionForm.add(value).then(res=>{
+    //   console.log('item added!');
+    //   }).catch(err=> console.log(err)
+    //   );
+  }
 
-   
-    this.submissionForm.add(value).then(res=>{
-      console.log('item added!');
-      }).catch(err=> console.log(err)
-      );
+get formControl(){
+    return this.ourForm['controls'];
+
+  }
+
+  resetForm(){
+    this.ourForm.reset();
+    this.ourForm.setValue({
+      type: '',
+      weight: '',
+
+      description : '' ,
+      price: '',
+      sellerid: '',
+      category: '',
+      picture:'',
+      shipping: ''
+    });
+    this.imgSrc= "../../../../assets/images/CategorySportGear.jpg";
+    this.selectedImage=null;
+    this.isSubmitted=false;
+
   }
 }
+
+
+
