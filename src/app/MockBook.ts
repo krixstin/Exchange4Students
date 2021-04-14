26884d259d3a64fd7cc46b01074bcf434a44e3a0
RedRoach51 <RedRoach51@gmail.com>
2021-04-14 01:55:57 -0400

    Expanded Mock dataset for books to demonstrate HTML/CSS on category page

--- src/app/MockBook.ts
+++ src/app/MockBook.ts
@@ -35,8 +35,50 @@ import {itemFurniture} from './ItemFurniture';
         // picture: 'Temp2',
         shipping: 'Temp2 shipping',
     }
-    const mockSport: itemSport = {
+    const mockBook3: itemBook = {
+        //passing objects ( array of obtions)
+        // [] for more than one Valliators
+        booktitle: 'Temp3 booktitle',
         itemID: '03',
+        description: 'Temp3 descrip',
+        edition: 301,
+        course: 'Temp3 course',
+        price: 302,
+        sellerid: 'Temp3 sellerid',
+        category: 'books',
+        // picture: 'Temp2',
+        shipping: 'Temp3 shipping',
+    }
+    const mockBook4: itemBook = {
+        //passing objects ( array of obtions)
+        // [] for more than one Valliators
+        booktitle: 'Temp4 booktitle',
+        itemID: '04',
+        description: 'Temp4 descrip',
+        edition: 401,
+        course: 'Temp4 course',
+        price: 402,
+        sellerid: 'Temp4 sellerid',
+        category: 'books',
+        // picture: 'Temp2',
+        shipping: 'Temp4 shipping',
+    }
+    const mockBook5: itemBook = {
+        //passing objects ( array of obtions)
+        // [] for more than one Valliators
+        booktitle: 'Temp2 booktitle',
+        itemID: '05',
+        description: 'Temp2 descrip',
+        edition: 201,
+        course: 'Temp2 course',
+        price: 202,
+        sellerid: 'Temp2 sellerid',
+        category: 'books',
+        // picture: 'Temp2',
+        shipping: 'Temp2 shipping',
+    }
+    const mockSport: itemSport = {
+        itemID: '06',
         description: 'Temp3 descrip',
         price:301,
         sellerid: 'Temp3 sellerid',
@@ -46,7 +88,7 @@ import {itemFurniture} from './ItemFurniture';
         weight: 302
     }
     const mockElectronic: itemElectronic = {
-        itemID: '04',
+        itemID: '07',
         description: 'Temp4 descrip',
         price:401,
         sellerid: 'Temp4 sellerid',
@@ -57,7 +99,7 @@ import {itemFurniture} from './ItemFurniture';
         
     }
     const mockFurniture: itemFurniture = {
-        itemID: '05',
+        itemID: '08',
         description: 'Temp5 descrip',
         price:501,
         sellerid: 'Temp5 sellerid',
@@ -70,7 +112,7 @@ import {itemFurniture} from './ItemFurniture';
 
     }
     const mockClothing: itemClothing = {
-        itemID: '06',
+        itemID: '09',
         description: 'Temp6 descrip',
         price:601,
         sellerid: 'Temp6 sellerid',
@@ -81,6 +123,19 @@ import {itemFurniture} from './ItemFurniture';
         color: 'Temp6 color'
     }
 
-        const Mock: item[] = [mockBook,mockBook2,mockSport,mockElectronic,mockFurniture,mockClothing]
+    const Mock: item[] = [mockBook,mockBook2,mockSport,mockElectronic,mockFurniture,mockClothing]
+
+    function newId(): string {
+    // Alphanumeric characters
+    const chars =
+      'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let autoId = '';
+    for (let i = 0; i < 20; i++) {
+      autoId += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    return autoId;
+  }
+
+}
 
 export{ Mock };
