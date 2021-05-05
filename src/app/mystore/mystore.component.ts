74a053b52ac0f6bb51684e3757e17ea0677ab49c
grinchpal <47698676+grinchpal@users.noreply.github.com>
2021-05-05 02:15:45 -0400

    update css/html some ts for mystore

--- src/app/mystore/mystore.component.ts
+++ src/app/mystore/mystore.component.ts
@@ -1,5 +1,9 @@
-import { Component, OnInit, ViewEncapsulation} from '@angular/core';
+
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router } from '@angular/router';
+import { item } from '../Item';
+import { ItemService } from '../item.service';
+import { Mock } from '../MockBook';
 
 @Component({
   selector: 'app-mystore',
@@ -8,9 +12,38 @@ import { Router } from '@angular/router';
   encapsulation: ViewEncapsulation.None
 })
 export class MystoreComponent implements OnInit {
-  constructor(public router: Router){
-  }
-  ngOnInit(): void {
-  }
+  
+  retrievedItems: item[];
+  items: item[];
+  sellerid = "elim1";
 
+  constructor(public router: Router, 
+             public ItemService: ItemService) { }
+
+
+  getItems(retrievedItems:item[]):void{
+
+    var removedItems: number[] = [];
+
+    console.log(retrievedItems)
+    for (const index in retrievedItems){
+      var item:item = retrievedItems[index]
+        if (item.sellerid !== this.sellerid){
+          removedItems.push(parseInt(index))
+        }
+    }
+
+    this.items = [];
+    for (const index in retrievedItems){
+      if (!removedItems.includes(parseInt(index))){
+        this.items.push(retrievedItems[index])
+      }
+    }
+
+   console.log(this.items)
+  }
+ 
+  ngOnInit(): void {
+    this.ItemService.getItems().subscribe((Items) => this.getItems(Items))
+  }
 }
